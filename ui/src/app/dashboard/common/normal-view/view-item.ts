46508664c0de342f0804aac3c2a9bf7083a11dd0
Peter Wood <peter.wood.za@gmail.com>
2020-02-08 15:30:12 +0200

    Removed unused file

--- ui/src/app/dashboard/common/normal-view/view-item.ts
+++
@@ -1,8 +0,0 @@
-
-import { ViewComponent } from "./view-component";
-import { Type } from "@angular/core";
-
-export class ViewItem {
-    constructor(public component: Type<ViewComponent>, public attributes?: any) {
-    }
-}
