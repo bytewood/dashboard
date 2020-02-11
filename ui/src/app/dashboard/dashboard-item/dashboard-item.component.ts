2c7b9b04772c0d3045fd17698cfb52565c4bdf65
Peter Wood <peter.wood.za@gmail.com>
2020-02-11 08:44:06 +0200

    Reworked maximize/restore but there is still an issue with ngx-charts growing in height

--- ui/src/app/dashboard/dashboard-item/dashboard-item.component.ts
+++ ui/src/app/dashboard/dashboard-item/dashboard-item.component.ts
@@ -1,54 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { DashboardItemMetadata } from "../metadata/dashboard-item-metadata";
-import { UiSyncService } from "../../ui-sync.service";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import {Component, HostBinding, ViewChild} from '@angular/core';
+import {DashboardItemMetadata} from "../metadata/dashboard-item-metadata";
+import {UiSyncService} from "../../ui-sync.service";
+import {IconProp} from "@fortawesome/fontawesome-svg-core";
 
 @Component({
     selector: 'app-item',
     template: ''
 })
-export abstract class DashboardItemComponent implements OnInit {
+export abstract class DashboardItemComponent {
     private readonly RESTORE_ICON = ['far', 'window-restore'] as IconProp;
     private readonly MINIMIZE_ICON = ['far', 'window-minimize'] as IconProp;
     private readonly MAXIMIZE_ICON = ['far', 'window-maximize'] as IconProp;
     private readonly CLOSE_ICON = ['far', 'window-close'] as IconProp;
 
-    private _maximized = false;
-
-    protected cardWidth = parent.innerWidth / 3;
-    protected cardHeight = this.cardWidth / 1.618;
+    @HostBinding("class.col-4") private restored = true;
+    @HostBinding("class.col-12") private maximized = false;
 
     data: any;
     metadata: DashboardItemMetadata;
 
     closeIcon = this.CLOSE_ICON;
     minimizeIcon = this.MINIMIZE_ICON;
-    resizeIcon = this.MAXIMIZE_ICON;
-
-    previousView = [];
-    view = [];
+    restoreIcon = this.RESTORE_ICON;
+    maximizeIcon = this.MAXIMIZE_ICON;
 
     protected constructor(private readonly sync: UiSyncService) {
     }
 
-    onResize() {
-        if (this._maximized) {
-            this._maximized = false;
-            this.resizeIcon = this.MAXIMIZE_ICON;
-            this.view = this.previousView;
-        } else {
-            this._maximized = true;
-            this.resizeIcon = this.RESTORE_ICON;
-            this.previousView = this.view;
-            this.view = [this.cardWidth, this.cardHeight];
-        }
+    onRestore() {
+        window.dispatchEvent(new Event("resize"));
+        this.restored = true;
+        this.maximized = false;
+    }
+
+    onMaximize() {
+        window.dispatchEvent(new Event("resize"));
+        this.restored = false;
+        this.maximized = true;
     }
 
     onClose() {
         this.sync.closeDashboardItem(this.metadata);
-    }
-
-    ngOnInit(): void {
-        this.previousView = this.view = [this.cardWidth, this.cardHeight];
     }
 }
